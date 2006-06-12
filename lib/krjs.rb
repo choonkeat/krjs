338821d6a9f25cd64a5adfaae5cb7e096c2fb061
choonkeat <choonkeat>
2006-06-12 10:37:24 +0000

    refactored to include observers need to overwrite :tag in order to append observers js after the tag - we cant do that solely with options now, appending _ZZ where ZZ is digits indicates the field or form will be observed at the interval

--- lib/krjs.rb
+++ lib/krjs.rb
@@ -28,15 +28,12 @@ module ActionView
         return nil if dom_id.nil?
         array = split_dom_id(dom_id)
         method_match = "on_#{array.first}_"
-        method_match += "(#{array[2]}|field)_" if not array[2].nil?
+        method_match += "(#{array[2]}|field|submit)_" if not array[2].nil?
         ctrler.methods.find{|x| x =~ /^#{method_match}(.+)$/}
       end
-    
-      def tag_options(options)
+          
+      def tag_options(options, viewer = nil, method_name = nil, event_attr = nil)
         # begin patch
-        viewer = self.respond_to?(:controller) ? self : self.template_object
-        method_name = controller_method(viewer.controller, options['id'])
-        event_attr = "on#{$1}" if method_name =~ /_([^_]+)$/
         if method_name && event_attr && options[event_attr].nil? 
           options[event_attr] = viewer.remote_function(
             :url => options.merge({
@@ -52,6 +49,37 @@ module ActionView
 
         cleaned_options = convert_booleans(options.stringify_keys.reject {|key, value| value.nil?})
           ' ' + cleaned_options.map {|key, value| %(#{key}="#{html_escape(value.to_s)}")}.sort * ' ' unless cleaned_options.empty?
+      end
+      
+      def tag(name, options = nil, open = false)
+        viewer = self.respond_to?(:controller) ? self : self.template_object
+        method_name = controller_method(viewer.controller, options['id'])
+        event_attr = "on#{$1}" if method_name =~ /_([^_]+(|_\d+))$/
+        appended = nil
+        if event_attr =~ /^on(\w+)_(\d+)$/
+          on_evt = $1
+          freq = $2
+          observe_options = {
+            :url => options.merge({
+              :action => method_name, 
+              :dom_id => options['id'],
+              :dom_index => split_dom_id(options['id'])[1],
+            }), 
+            :with => "'dom_value=' + Form.serialize($('#{options['id']}'))",
+            :frequency => freq.to_i,
+          }          
+          if on_evt =~ /(form|submit)/
+            appended = observe_form(options['id'], observe_options)
+          else
+            appended = observe_field(options['id'], observe_options.merge({
+                :with => "'dom_value=' + $('#{options['id']}').value",
+                :on => on_evt,
+                })
+            )
+          end
+          method_name = nil; event_attr = nil # let tag_options behave normally
+        end
+        "<#{name}#{tag_options(options.stringify_keys, viewer, method_name, event_attr) if options}" + (open ? ">" : " />") + appended.to_s
       end
     end
   end 
